fd3759483a2582a3c3168e072da0e8a29a863a65
zhaifangzhi <zhaifangyuan2@gmail.com>
2019-07-08 16:30:00 +0800

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 
-<title>PHP-Proxy</title>
+<title>My Proxy</title>
 
 <meta name="generator" content="php-proxy.com">
 <meta name="version" content="<?=$version;?>">
@@ -51,7 +51,7 @@ html body {
 <div id="container">
 
 	<div style="text-align:center;">
-		<h1 style="color:blue;">PHP-Proxy</h1>
+		<h1 style="color:blue;">My Proxy</h1>
 	</div>
 	
 	<?php if(isset($error_msg)){ ?>
